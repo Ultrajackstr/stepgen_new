1bd1f7bccacfaff263ba6836dd580f4de28012fb
Giacomo Gropplero <giacomo@gropplero.com>
2023-02-15 14:56:29 +0100

    Migrate to fixed crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,190 +1,152 @@
 #![no_std]
-#![deny(warnings)]
 
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub enum Error {
-    TooSlow,
-    TooFast,
-    SpeedAccelerationNotSet,
-}
+use fixed::FixedU32;
+use fixed::types::extra::{U0, U12, U18, U32};
+use fixed::types::U20F12;
+use fixed_macro::fixed;
+use fixed_sqrt::FixedSqrt;
 
-pub type Result = core::result::Result<(), Error>;
+type Fix = FixedU32<U12>;
+type Fix18 = FixedU32<U18>;
+// Higher precision for the sqrt function.
+type Fix32 = FixedU32<U32>;
+// Higher precision for the sqrt function.
+type Fix0 = FixedU32<U0>; // Equivalent to u32.
+
+const ZERO_POINT_TWENTY_SIX: U20F12 = fixed!(0.26: U20F12);
+const TWO: U20F12 = fixed!(2: U20F12);
+const FOUR: U20F12 = fixed!(4: U20F12);
 
 /// State of the stepgen.
 #[derive(Debug)]
-pub struct Stepgen {
-    // Current step
-    current_step: u32,
-
+pub struct Stepgen<const TIMER_HZ_MICROS: u32> {
+    current_step: Fix0,
     // Amount of acceleration steps we've taken so far
-    speed: u32,
-    // Previously calculated delay, in 16.16 format
-    delay: u32,
-
+    acceleration_steps: Fix,
+    // Previously calculated delay
+    current_delay: Fix,
     // If slewing, this will be the slewing delay. Switched to this mode once
-    // we overshoot target speed. 16.16 format.
-    slewing_delay: u32,
-
-    // Timer frequency
-    ticks_per_second: u32,
-    // First step delay, in 16.16 format
-    first_delay: u32,
+    // we overshoot target speed.
+    slewing_delay: Fix,
+    // First step delay
+    first_delay: Fix,
     // Target step
-    target_step: u32,
-    // Target speed delay, in 16.16 format
-    target_delay: u32,
+    target_step: Fix0,
+    // Target speed delay
+    target_delay: Fix,
 }
 
-impl Stepgen {
-    /// Create new copy of stepgen. `ticks_per_second` defines size of each tick stepgen operates.
-    /// All settings (acceleration, speed) and current parameters (speed) are defined in terms of
-    /// these ticks.
-    pub const fn new(ticks_per_second: u32) -> Stepgen {
+impl<const TIMER_HZ_MICROS: u32> Stepgen<TIMER_HZ_MICROS> {
+    /// Create new copy of stepgen.
+    pub fn new(target_rpm: u16, accel: u16, target_step: u32) -> Stepgen<TIMER_HZ_MICROS> {
+        if !(150..=4_800).contains(&accel) || target_rpm < 32 {
+            return Stepgen {
+                current_step: Fix0::ZERO,
+                acceleration_steps: Fix::from_num(150),
+                current_delay: Fix::ZERO,
+                slewing_delay: Fix::ZERO,
+                first_delay: Fix::ZERO,
+                target_step: Fix0::ZERO,
+                target_delay: Fix::ZERO,
+            };
+        }
+        // Convert target RPM to delay in timer ticks.
+        let target_delay: Fix = Fix::from_num(60) / Fix::from_num(200) * Fix::from_num(TIMER_HZ_MICROS) / Fix::from_num(target_rpm);
+        // Calculate first delay based on acceleration.
+        let first_delay: Fix = Fix::from_num(Fix32::from_num(Fix18::from_num(2u8) / (Fix18::from_num(accel) * Fix18::from_num(3))).sqrt()
+            * Fix32::from_num(0.676)) * Fix::from_num(TIMER_HZ_MICROS);
         Stepgen {
-            current_step: 0,
-            speed: 0,
-            delay: 0,
-            slewing_delay: 0,
-            ticks_per_second,
-            first_delay: 0,
-            target_step: 0,
-            target_delay: 0,
+            current_step: Fix0::ZERO,
+            acceleration_steps: Fix::from_num(0),
+            current_delay: Fix::from_num(0),
+            slewing_delay: Fix::from_num(0),
+            first_delay,
+            target_step: Fix0::from_num(target_step),
+            target_delay,
         }
-    }
-
-    pub fn set_acceleration(&mut self, acceleration: f32) -> Result {
-        let delay = self.ticks_per_second as f32 * micromath::F32Ext::sqrt(2.0 / acceleration) * 0.676;
-        self.first_delay = delay as u32;
-        Ok(())
-    }
-
-    pub fn set_target_step(&mut self, target_step: u32) -> Result {
-        if self.target_delay == 0 || self.first_delay == 0 {
-            return Err(Error::SpeedAccelerationNotSet);
-        }
-        self.target_step = target_step;
-        Ok(())
-    }
-
-    pub fn set_target_speed(&mut self, target_speed: f32) -> Result {
-        // if target_speed == 0_f32 {
-        //     // Too slow, speed is zero
-        //     return Err(Error::TooSlow);
-        // }
-        let delay = self.ticks_per_second as f32 / target_speed;
-        self.target_delay = delay as u32;
-        Ok(())
-    }
-
-    /// Current step stepgen is at.
-    pub fn current_step(&self) -> u32 {
-        self.current_step
-    }
-
-    /// Target step stepgen should stop at. Note that if stepper is running too fast, it might not
-    /// be able to stop exactly at this step. This could happen when target step is updated after
-    /// stepper motor accelerated to certain speed.
-    pub fn target_step(&self) -> u32 {
-        self.target_step
-    }
-
-    /// Get estimated current speed, in 24.8 format
-    pub fn current_speed(&self) -> u32 {
-        let delay = if self.slewing_delay != 0 { self.slewing_delay } else { self.delay };
-        if delay != 0 {
-            self.ticks_per_second / delay
-        } else {
-            0
-        }
-    }
-
-    /// If we are running at target speed
-    pub fn is_at_speed(&self) -> bool {
-        self.slewing_delay != 0
     }
 
     /// Returns '0' if should stop. Otherwise, returns timer delay in 24.8 format
     fn next_delay(&mut self) -> u32 {
-        let target_step = self.target_step;
-        let target_delay = self.target_delay;
-        let st = self.current_step;
-
         // We are at the stop point and speed is zero -- return "stopped" (delay of 0)
-        if st >= target_step && self.speed <= 1 {
-            self.speed = 0;
+        if self.current_step >= self.target_step && self.acceleration_steps <= Fix::ONE {
+            self.acceleration_steps = Fix::ZERO;
             return 0;
         }
 
         // Stop slewing if target delay was changed
-        if self.slewing_delay != 0 && self.slewing_delay != target_delay {
-            self.slewing_delay = 0;
+        if self.slewing_delay != Fix::ZERO && self.slewing_delay != self.target_delay {
+            self.slewing_delay = Fix::ZERO;
         }
 
         // Steps made so far
-        self.current_step += 1;
+        self.current_step += Fix0::ONE;
 
-        if self.speed == 0 {
-            return if target_delay > self.first_delay {
+        if self.acceleration_steps == Fix::ZERO {
+            return if self.target_delay > self.first_delay {
                 // No acceleration is necessary -- just return the target delay
-                target_delay
+                self.target_delay.to_num::<u32>()
             } else {
                 // First step: load first delay, count as one acceleration step
-                self.delay = self.first_delay;
-                self.speed = 1;
-                self.delay
+                self.current_delay = self.first_delay;
+                self.acceleration_steps = Fix::ONE;
+                self.current_delay.to_num::<u32>()
             };
         }
 
         // Calculate the projected step we would stop at if we start decelerating right now
-        let est_stop = st + self.speed;
-        if est_stop == target_step {
+        let estimated_stop_step = self.current_step + self.acceleration_steps.to_num::<Fix0>();
+        if estimated_stop_step == self.target_step {
             // We would stop one step earlier than we want, so let's just
             // return the same delay as the current one and start deceleration
             // on the next step.
-        } else if est_stop > target_step {
+        } else if estimated_stop_step > self.target_step {
             // We need to stop at target step, slow down
             self.slowdown();
 
             // We are not slewing even though we could have slowed down below the slewing speed
-            self.slewing_delay = 0;
-        } else if self.slewing_delay == 0 && self.delay < target_delay {
+            self.slewing_delay = Fix::ZERO;
+        } else if self.slewing_delay == Fix::ZERO && self.current_delay < self.target_delay {
             // Not slewing and running too fast, slow down
             self.slowdown();
 
             // Switch to slewing if we slowed down enough
-            if self.delay >= target_delay {
-                self.slewing_delay = target_delay;
+            if self.current_delay >= self.target_delay {
+                self.slewing_delay = self.target_delay;
             }
-        } else if self.slewing_delay == 0 && self.delay > target_delay {
+        } else if self.slewing_delay == Fix::ZERO && self.current_delay > self.target_delay {
             // Not slewing and running too slow, speed up
             self.speedup();
 
             // Switch to slewing if we have accelerated enough
-            if self.delay <= target_delay {
-                self.slewing_delay = target_delay;
+            if self.current_delay <= self.target_delay {
+                self.slewing_delay = self.target_delay;
             }
         }
 
         // If slewing, return slew delay. delay should be close enough, but could
         // be different due to the accumulated rounding errors
-        if self.slewing_delay != 0 { self.slewing_delay } else { self.delay }
+        if self.slewing_delay != Fix::ZERO { self.slewing_delay.to_num::<u32>() } else { self.current_delay.to_num::<u32>() }
     }
 
 
     fn speedup(&mut self) {
-        let denom = 4 * self.speed + 1;
-        self.delay -= (2 * self.delay + denom) / denom;
-        self.speed += 1;
+        let denom = FOUR * self.acceleration_steps + Fix::ONE;
+        self.current_delay -= (TWO * self.current_delay) / denom;
+        self.acceleration_steps += Fix::ONE;
     }
 
     fn slowdown(&mut self) {
-        self.speed -= 1;
-        let denom = 4 * self.speed - 1;
-        self.delay += (2 * self.delay + denom) / denom;
+        if self.acceleration_steps < ZERO_POINT_TWENTY_SIX { // Prevent underflow.
+            self.acceleration_steps = ZERO_POINT_TWENTY_SIX
+        }
+        self.acceleration_steps -= Fix::ONE;
+        let denom = FOUR * self.acceleration_steps - Fix::ONE;
+        self.current_delay += (TWO * self.current_delay) / denom;
     }
 }
 
-impl Iterator for Stepgen {
+impl<const TIMER_HZ_MICROS: u32> Iterator for Stepgen<TIMER_HZ_MICROS> {
     type Item = u32;
 
     fn next(&mut self) -> Option<Self::Item> {
